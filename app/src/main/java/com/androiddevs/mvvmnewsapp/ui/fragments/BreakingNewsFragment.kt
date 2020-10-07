a9d32dd67d6827a3f2dd073dc2d78817782297e3
caron <caronf82@gmail.com>
2020-10-07 18:32:34 +0530

    handling breaking news response

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/BreakingNewsFragment.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/BreakingNewsFragment.kt
@@ -2,20 +2,66 @@ package com.androiddevs.mvvmnewsapp.ui.fragments
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.androiddevs.mvvmnewsapp.R
+import com.androiddevs.mvvmnewsapp.adapters.NewsAdapter
 import com.androiddevs.mvvmnewsapp.ui.NewsActivity
 import com.androiddevs.mvvmnewsapp.ui.NewsViewModel
+import com.androiddevs.mvvmnewsapp.ui.Resource
+import kotlinx.android.synthetic.main.fragment_breaking_news.*
 
 class  BreakingNewsFragment :Fragment(R.layout.fragment_breaking_news) {
 
     lateinit var viewModel: NewsViewModel
+    lateinit var newsAdapter:NewsAdapter
+    val TAG="BreakingNewsFragment"
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel= (activity as NewsActivity).viewModel as NewsViewModel
+        setupRecyclerView()
+        viewModel.breakingNews.observe(viewLifecycleOwner, Observer { response->
+            when(response){
+                is Resource.Success -> {
+                    hideProgressBar()
+                    response.data?.let {
+                        newsResponse ->
+                        newsAdapter.differ.submitList(newsResponse.articles)
+                    }
+                }
+                is Resource.Error ->{
+                    hideProgressBar()
+                    response.message?.let{
+                        message -> Log.e(TAG,"An error has occured: $message")
+                    }
+                }
+                is Resource.Loading ->{
+                    showProgressBar()
+                }
+            }
+        })
+    }
+
+    private fun setupRecyclerView(){
+        newsAdapter= NewsAdapter()
+        rvBreakingNews.apply {
+            adapter=newsAdapter
+            layoutManager=LinearLayoutManager(activity)
+
+
+        }
+    }
+
+    private fun hideProgressBar(){
+        paginationProgressBar.visibility-View.INVISIBLE
+    }
+    private fun showProgressBar(){
+        paginationProgressBar.visibility-View.VISIBLE
     }
 }
--- app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -2,7 +2,7 @@ package com.androiddevs.mvvmnewsapp.util
 
 class Constants {
     companion object{
-        const val API_KEY="d5e60452a66f470388d880f0195dac"
+        const val API_KEY="d5e60452a66f470388d880f0195dac96"
         const val BASE_URL="https://newsapi.org/"
     }
 }
