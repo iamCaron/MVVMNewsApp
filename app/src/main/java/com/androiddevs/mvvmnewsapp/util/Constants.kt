58ad0b62c62d81e81badb49b721091a847eb9466
caron <caronf82@gmail.com>
2020-10-07 10:56:03 +0530

    added retrofit and api key to prepare for the network request

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY="d5e60452a66f470388d880f0195dac"
+        const val BASE_URL="https://newsapi.org/"
+    }
+}
