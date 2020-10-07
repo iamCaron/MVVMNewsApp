7c7a4db3aafd14293080e82f5833471f0ec98663
caron <caronf82@gmail.com>
2020-10-07 15:54:16 +0530

    defined mvvm architecture skeleton

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/BreakingNewsFragment.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/BreakingNewsFragment.kt
@@ -1,8 +1,21 @@
 package com.androiddevs.mvvmnewsapp.ui.fragments
 
+import android.app.Activity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.androiddevs.mvvmnewsapp.R
+import com.androiddevs.mvvmnewsapp.ui.NewsActivity
+import com.androiddevs.mvvmnewsapp.ui.NewsViewModel
 
-class BreakingNewsFragment :Fragment(R.layout.fragment_breaking_news) {
+class  BreakingNewsFragment :Fragment(R.layout.fragment_breaking_news) {
 
+    lateinit var viewModel: NewsViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel= (activity as NewsActivity).viewModel as NewsViewModel
+    }
 }
